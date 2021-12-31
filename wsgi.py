5dc4f45816e8876516badbcc488fb7f79a9ec0f3
Jescs <515199703@qq.com>
2021-12-31 09:44:24 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -9,30 +9,6 @@ def index():
     return render_template("index.html")
 
 
-@application.route('/pirate')
-def pirate():
-    return render_template("pirate.html")
-
-
-@application.route('/zombie')
-def zombie():
-    return render_template("zombie.html")
-
-
-@application.route('/legal')
-def legal():
-    return render_template("legal.html")
-
-
-@application.route('/isready')
-def isready():
-    return 'isReady'
-
-
-@application.route('/isalive')
-def isalive():
-    return 'isAlive'
-
 
 if __name__ == "__main__":
     application.run()
